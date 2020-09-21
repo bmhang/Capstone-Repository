38a244053c57a9c2b17a60154684de408c468611
Brandon Hang <bmhang13@gmail.com>
2020-09-21 10:50:21 -0700

    Uploaded Empty Unity Project
    Uploaded new Unity project called "CS Capstone Project"

---
+++ CS Capstone Project/Assets/TestTextScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestTextScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(0, 20 * Time.deltaTime, 0, Space.World);
+    }
+}
