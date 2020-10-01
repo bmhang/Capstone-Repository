66a275b7c8258cf2f3c3d05be6272c262bc28c37
Brandon Hang <bmhang13@gmail.com>
2020-09-30 23:20:12 -0700

    Added Emotiv Plugin
    Added the Emotiv Plugin and playing with getting a headset added. Doesn't work yet.

---
+++ CS Capstone Project/Assets/TestEmotivScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using EmotivUnityPlugin;
+using UnityEngine;
+
+public class TestEmotivScript : MonoBehaviour
+{
+    DataStreamManager _dataStream = DataStreamManager.Instance;
+
+    void Start()
+    {
+        _dataStream.StartAuthorize("");
+        StartCoroutine(checkForHeadsets());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator checkForHeadsets() {
+        while(true) {
+            List<Headset> detectedHeadset = _dataStream.GetDetectedHeadsets();
+            print(detectedHeadset.Count);
+            yield return new WaitForSecondsRealtime(0.5f);
+        }
+    }
+}
