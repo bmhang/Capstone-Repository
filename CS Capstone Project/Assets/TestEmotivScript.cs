93485a64ff3964a04d5767296cf1a3a1cc580c8d
Brandon Hang <bmhang13@gmail.com>
2020-10-01 21:41:24 -0700

    More subtle changes
    No luck though

--- CS Capstone Project/Assets/TestEmotivScript.cs
+++ CS Capstone Project/Assets/TestEmotivScript.cs
@@ -8,6 +8,11 @@ public class TestEmotivScript : MonoBehaviour
     List<Headset> headsets = new List<Headset>();
     bool headsetFound = false;
     bool headsetSubscribed = false;
+    bool isConnectDone = false;
+    ConnectToCortexStates _lastState;
+
+    const float TIME_QUERY_HEADSET = 2.0f;
+    float _timerCounter_queryHeadset = 0;
 
     void Start()
     {
@@ -18,7 +23,64 @@ public class TestEmotivScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _timerCounter_queryHeadset += Time.deltaTime;
+        if(_timerCounter_queryHeadset > TIME_QUERY_HEADSET) {
+            _timerCounter_queryHeadset -= TIME_QUERY_HEADSET;
+            List<Headset> detectedHeadset = DataStreamManager.Instance.GetDetectedHeadsets();
+            print(detectedHeadset.Count);
+        }
 
+        if(isConnectDone)
+            return;
+
+        var curState = DataStreamManager.Instance.GetConnectToCortexState();
+        if (_lastState == curState)
+                return;
+
+        // curState = ConnectToCortexStates.License_HardLimited; // TODO: only for test now
+        _lastState = curState;
+        switch (curState) {
+            case ConnectToCortexStates.Service_connecting: {
+                // _stateText.text = "Connecting To service..."; // TODO: check font size
+                Debug.Log("=============== Connecting To service");
+                break;
+            }
+            case ConnectToCortexStates.EmotivApp_NotFound: {
+                Debug.Log("=============== Connect_failed");
+                break;
+            }
+            case ConnectToCortexStates.Login_waiting: {
+                Debug.Log("=============== Login_waiting");
+                break;
+            }
+            case ConnectToCortexStates.Login_notYet: {
+                Debug.Log("=============== Login_notYet");
+                break;
+            }
+            case ConnectToCortexStates.Authorizing: {
+                Debug.Log("=============== Authorizing");
+                break;
+            }
+            case ConnectToCortexStates.Authorize_failed: {
+                Debug.Log("=============== Authorize_failed");
+                break;
+            }
+            case ConnectToCortexStates.Authorized: {
+                isConnectDone = true;
+                Debug.Log("=============== Authorized");
+                break;
+            }
+            case ConnectToCortexStates.LicenseExpried: {
+                isConnectDone = true;
+                Debug.Log("=============== Trial expired");
+                break;
+            }
+            case ConnectToCortexStates.License_HardLimited: {
+                isConnectDone = true;
+                Debug.Log("=============== License_HardLimited");
+                break;
+            }
+        }
     }
 
     void OnApplicationQuit()
