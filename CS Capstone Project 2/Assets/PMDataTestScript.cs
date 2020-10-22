6d02875262c8415d37198feeae391cb09e15c6b8
poojapatel45 <32950431+poojapatel45@users.noreply.github.com>
2020-10-22 13:47:30 -0700

    Update PMDataTestScript.cs to collect data for all 6 performance metrics

--- CS Capstone Project 2/Assets/PMDataTestScript.cs
+++ CS Capstone Project 2/Assets/PMDataTestScript.cs
@@ -9,6 +9,11 @@ public class PMDataTestScript : MonoBehaviour
     public Light directionalLight;
 
     private float engagement = -1;
+    private float excitment = -1;
+    private float focus = -1;
+    private float interest = -1;
+    private float relaxation = -1;
+    private float stress = -1;
 
     // Start is called before the first frame update
     void Start()
@@ -28,11 +33,36 @@ public class PMDataTestScript : MonoBehaviour
                     engagement = (float)data;
                 }
 
+                if(chanStr ==  "exc" && data > -1) {
+                    excitment = (float)data;
+                }
+
+                if(chanStr == "foc" && data > -1) {
+                    focus = (float)data;
+                }
+
+                if(chanStr == "int" && data > -1) {
+                    interest = (float)data;
+                }
+
+                if(chanStr ==  "rel" && data > -1) {
+                    relaxation = (float)data;
+                }
+
+                if(chanStr == "str" && data > -1) {
+                    stress = (float)data;
+                }
+
                 print("Stream: " + chanStr + " " + "Value: " + data);
             }
         }
 
         directionalLight.transform.eulerAngles = new Vector3(360 * engagement, 0, 0);
         print("ENGAGEMENT: " + engagement);
+        print("EXCITMENT: " + excitment);
+        print("FOCUS: " + focus);
+        print("INTEREST: " + interest);
+        print("RELAXATION: " + relaxation);
+        print("STRESS: " + stress);
     }
 }
