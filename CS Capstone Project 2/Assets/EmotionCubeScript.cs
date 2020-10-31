87cbea03f1d6b2f1838a2acbe24bd11a3c00fdcd
Brandon Hang <44913259+bmhang@users.noreply.github.com>
2020-10-31 11:59:28 -0700

    One Last touch to Make the Cube Float

--- CS Capstone Project 2/Assets/EmotionCubeScript.cs
+++ CS Capstone Project 2/Assets/EmotionCubeScript.cs
@@ -8,6 +8,8 @@ public class EmotionCubeScript : MonoBehaviour
     private float smooth;
     public float durationTime;
 
+    private float floatTime;
+
     //This is a sphere
     public GameObject sphere;
     public GameObject cube;
@@ -21,9 +23,16 @@ public class EmotionCubeScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Rotating the cube by durationTime
         smooth = Time.deltaTime * durationTime;
         this.gameObject.transform.Rotate(new Vector3(vectorValues[0], vectorValues[1], vectorValues[2]) * smooth);
 
-        
+        //Making the cube float up and down
+        floatTime += Time.deltaTime * 1;
+        if(floatTime >= 2 * Mathf.PI)
+        {
+            floatTime = 0;
+        }
+        this.transform.position = new Vector3(this.transform.position.x, Mathf.Sin(floatTime) / 12 + 1.8f, this.transform.position.z);
     }
 }
