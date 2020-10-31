8eef64f459a4cfa40737cacd32e7479686e52b9e
Brandon Hang <44913259+bmhang@users.noreply.github.com>
2020-10-31 11:18:44 -0700

    Added Emotion Cube

---
+++ CS Capstone Project 2/Assets/EmotionCubeScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmotionCubeScript : MonoBehaviour
+{
+    public int[] vectorValues = new int[] { 1, 0, 0 };
+    private float smooth;
+    public float durationTime;
+
+    //This is a sphere
+    public GameObject sphere;
+    public GameObject cube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        smooth = Time.deltaTime * durationTime;
+        cube.gameObject.transform.Rotate(new Vector3(vectorValues[0], vectorValues[1], vectorValues[2]) * smooth);
+
+        
+    }
+}
