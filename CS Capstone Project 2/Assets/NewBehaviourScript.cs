3392ee8c11a89e9d9e6c7f1e0cd0d37eb0e5f925
sjloucks <46513109+sjloucks@users.noreply.github.com>
2021-03-17 12:59:45 -0700

    volume changes

--- CS Capstone Project 2/Assets/NewBehaviourScript.cs
+++ CS Capstone Project 2/Assets/NewBehaviourScript.cs
@@ -13,6 +13,11 @@ public class NewBehaviourScript : MonoBehaviour
     private float[] frames = new float[n];
     private int previousChange = 1;
 
+    private int numExcitement = 0;
+    private float[] excitement = new float[n];
+    private float[] excitementTime = new float[n];
+    private int previousExcitement = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,13 +41,15 @@ public class NewBehaviourScript : MonoBehaviour
                     if (slope > 0.01)
                     {
                         LightChangerScript.mainTableLight_Intensity = LightChangerScript.mainTableLight_Intensity + (previousChange * slope * 100);//experiment to find good factor
-                        LightChangerScript.crystalLight1_Intensity = LightChangerScript.crystalLight1_Intensity + (previousChange * slope * 100);
+                        LightChangerScript.crystalLight3_Intensity = LightChangerScript.crystalLight3_Intensity + (previousChange * slope * 100);
+                        LightChangerScript.crystalLight4_Intensity = LightChangerScript.crystalLight4_Intensity + (previousChange * slope * 100);
                         previousChange = 1;
                     }
                     else if(slope < -.01)
                     {
                         LightChangerScript.mainTableLight_Intensity = LightChangerScript.mainTableLight_Intensity + (previousChange * slope * 100);//experiment to find good factor
-                        LightChangerScript.crystalLight1_Intensity = LightChangerScript.crystalLight1_Intensity + (previousChange * slope * 100);
+                        LightChangerScript.crystalLight3_Intensity = LightChangerScript.crystalLight3_Intensity + (previousChange * slope * 100);
+                        LightChangerScript.crystalLight4_Intensity = LightChangerScript.crystalLight4_Intensity + (previousChange * slope * 100);
                         previousChange = -1;
                     }
                     print("Slope: " + slope);
@@ -56,7 +63,43 @@ public class NewBehaviourScript : MonoBehaviour
                     print("Relaxation" + i + ": " + relaxation[i]);
                     print("Time" + i + frames[i]);
                 }
-            } 
+            }
+
+        if (pmDataCatcher.excitment != -1 && (numExcitement == 0 || excitement[numExcitement - 1] != pmDataCatcher.excitment)) //deal with delay from simulator
+        {
+            //frames[numData] = Time.deltaTime; Doesn't give right time
+            excitementTime[numExcitement] = numExcitement;
+            excitement[numExcitement] = pmDataCatcher.excitment;
+
+            //if full array, calculate regression and change light accordingly
+            if (numExcitement == n - 1)
+            {
+                float slope = Regression(excitementTime, excitement);
+
+                if (slope > 0.001)
+                {
+                    AudioChanger.music_intensity = AudioChanger.music_intensity + (previousExcitement * slope * 10);//experiment to find good factor
+                    AudioChanger.cafeMusic_intensity = AudioChanger.cafeMusic_intensity + (previousExcitement * slope * 10);
+                    previousExcitement = 1;
+                }
+                else if (slope < -.001)
+                {
+                    AudioChanger.music_intensity = AudioChanger.music_intensity + (previousExcitement * slope * 1000);//experiment to find good factor
+                    AudioChanger.cafeMusic_intensity = AudioChanger.cafeMusic_intensity + (previousExcitement * slope * 1000);
+                    previousExcitement = -1;
+                }
+                print("Slope: " + slope);
+                numExcitement = -1;
+            }
+
+            numExcitement++;
+            print("Frame Excitement: " + numExcitement);
+            for (int i = 0; i < n; i++)
+            {
+                print("Excitement" + i + ": " + excitement[i]);
+                print("Time" + i + excitementTime[i]);
+            }
+        }
     }
 
     float Regression(float[] xdata, float[] ydata)
